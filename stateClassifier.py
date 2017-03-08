4676d6af6a56a24b0b2f98ac0cdb73528403cd2c
hillele <elexhill@gmail.com>
2017-03-08 13:31:07 -0800

    Updated Comments

--- stateClassifier.py
+++ stateClassifier.py
@@ -1,6 +1,10 @@
+# Define which workbook you want to classify
+currWorkbook = 'USStates.xlsx'
+newWorkbook = 'CategorizedUSStates.xlsx'
+
 import openpyxl
 
-wb = openpyxl.load_workbook('USStates.xlsx')
+wb = openpyxl.load_workbook(currWorkbook)
 names = wb.get_sheet_names()
 sheet = wb.get_sheet_by_name(names[0])
 
@@ -11,16 +15,17 @@ possibleInputs = ['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'col
 matchAbbrev = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','DC','DC','DC','WA','WV','WI','WY','DC','DC','AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT', 'VA','WA','WV','WI','WY', 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL','GA','HI','ID','IL','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT', 'VA','WA','WV','WI','WY', 'LA', 'LA', 'TN', 'NV', 'TX', 'TN', 'VA',
                'NONE', 'NONE', 'NONE', 'NONE', 'NONE']
 
-# Check to make sure these all match up appropriately
-i = 0
-while i < len(possibleInputs):
-    print possibleInputs[i] + ", " + matchAbbrev[i]
-    i += 1
+# Use this code to check to make sure these all match up appropriately
+#i = 0
+#while i < len(possibleInputs):
+#    print possibleInputs[i] + ", " + matchAbbrev[i]
+#    i += 1
 
 # Initialize count of State Abbreviation tracker
 st_Abbrev = {'NONE': 0,'AL': 0, 'AK': 0, 'AZ': 0, 'AR': 0, 'CA': 0, 'CO': 0, 'CT': 0, 'DE': 0, 'FL': 0, 'GA': 0, 'HI': 0, 'ID': 0, 'IL': 0, 'IN': 0, 'IA': 0, 'KS': 0, 'KY': 0, 'LA': 0, 'ME': 0, 'MD': 0, 'MA': 0, 'MI': 0, 'MN': 0, 'MS': 0, 'MO': 0, 'MT': 0, 'NE': 0, 'NV': 0, 'NH': 0, 'NJ': 0, 'NM': 0, 'NY': 0, 'NC': 0, 'ND': 0, 'OH': 0, 'OK': 0, 'OR': 0, 'PA': 0, 'RI': 0, 'SC':0, 'SD':0, 'TN':0, 'TX':0, 'UT':0, 'VT':0, 'VA': 0, 'WA':0, 'WV':0, 'WI':0, 'WY':0, 'DC':0}
 
-# Do the thing
+# Sometimes only one of these loops works on different computers. If the current loop is giving you an error, try this one:
+# for row in sheet.columns[0]:
 currRow = 1
 for row in sheet['A']:
     i = 0
@@ -44,7 +49,7 @@ for row in sheet['A']:
     currRow += 1
 
 # Save File
-wb.save(filename = 'CategorizedUSStates.xlsx')
+wb.save(filename = newWorkbook)
 
 # Print count of each state
 print "Counts:"        
